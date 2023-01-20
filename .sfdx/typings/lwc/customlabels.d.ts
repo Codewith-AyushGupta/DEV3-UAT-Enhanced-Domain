1b7be2da8770bdda4b950d1829c8cb51b25dc009
Ayush gupta <ayush.gupta@nubystech.com>
2023-01-20 15:12:41 +0530

    2nd commit

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,172 @@
+declare module "@salesforce/label/c.AssignBtn" {
+    var AssignBtn: string;
+    export default AssignBtn;
+}
+declare module "@salesforce/label/c.AssignSuccessSummary" {
+    var AssignSuccessSummary: string;
+    export default AssignSuccessSummary;
+}
+declare module "@salesforce/label/c.AssignSuccessTitle" {
+    var AssignSuccessTitle: string;
+    export default AssignSuccessTitle;
+}
+declare module "@salesforce/label/c.AssignToBtn" {
+    var AssignToBtn: string;
+    export default AssignToBtn;
+}
+declare module "@salesforce/label/c.AssigningBtn" {
+    var AssigningBtn: string;
+    export default AssigningBtn;
+}
+declare module "@salesforce/label/c.Attach_copy_of_exemption_certificate" {
+    var Attach_copy_of_exemption_certificate: string;
+    export default Attach_copy_of_exemption_certificate;
+}
+declare module "@salesforce/label/c.Balance_Sheet" {
+    var Balance_Sheet: string;
+    export default Balance_Sheet;
+}
+declare module "@salesforce/label/c.CloseDateCriteriaLabel" {
+    var CloseDateCriteriaLabel: string;
+    export default CloseDateCriteriaLabel;
+}
+declare module "@salesforce/label/c.Credit_Application_Form_URL" {
+    var Credit_Application_Form_URL: string;
+    export default Credit_Application_Form_URL;
+}
+declare module "@salesforce/label/c.CriteriaSearchSection" {
+    var CriteriaSearchSection: string;
+    export default CriteriaSearchSection;
+}
+declare module "@salesforce/label/c.Decor_URL" {
+    var Decor_URL: string;
+    export default Decor_URL;
+}
+declare module "@salesforce/label/c.DisplayViewLabel" {
+    var DisplayViewLabel: string;
+    export default DisplayViewLabel;
+}
+declare module "@salesforce/label/c.HeaderText" {
+    var HeaderText: string;
+    export default HeaderText;
+}
+declare module "@salesforce/label/c.Income_Statement" {
+    var Income_Statement: string;
+    export default Income_Statement;
+}
+declare module "@salesforce/label/c.KeyReportHomeLink" {
+    var KeyReportHomeLink: string;
+    export default KeyReportHomeLink;
+}
+declare module "@salesforce/label/c.Key_Report_Type" {
+    var Key_Report_Type: string;
+    export default Key_Report_Type;
+}
+declare module "@salesforce/label/c.Most_Recent_Years_Tax_Returns" {
+    var Most_Recent_Years_Tax_Returns: string;
+    export default Most_Recent_Years_Tax_Returns;
+}
+declare module "@salesforce/label/c.NewViewLinkLabel" {
+    var NewViewLinkLabel: string;
+    export default NewViewLinkLabel;
+}
+declare module "@salesforce/label/c.NoResultSummary" {
+    var NoResultSummary: string;
+    export default NoResultSummary;
+}
+declare module "@salesforce/label/c.NoResultTitle" {
+    var NoResultTitle: string;
+    export default NoResultTitle;
+}
+declare module "@salesforce/label/c.NoViewSelection" {
+    var NoViewSelection: string;
+    export default NoViewSelection;
+}
+declare module "@salesforce/label/c.Opportunity_Split_Type_Revenue_Id" {
+    var Opportunity_Split_Type_Revenue_Id: string;
+    export default Opportunity_Split_Type_Revenue_Id;
+}
+declare module "@salesforce/label/c.Other_Document" {
+    var Other_Document: string;
+    export default Other_Document;
+}
+declare module "@salesforce/label/c.Preferred_Vendor_RPF_link" {
+    var Preferred_Vendor_RPF_link: string;
+    export default Preferred_Vendor_RPF_link;
+}
+declare module "@salesforce/label/c.Register_Job_Cc_email" {
+    var Register_Job_Cc_email: string;
+    export default Register_Job_Cc_email;
+}
+declare module "@salesforce/label/c.Register_Job_Email" {
+    var Register_Job_Email: string;
+    export default Register_Job_Email;
+}
+declare module "@salesforce/label/c.Report_for_Home" {
+    var Report_for_Home: string;
+    export default Report_for_Home;
+}
+declare module "@salesforce/label/c.ResultBlockTitle" {
+    var ResultBlockTitle: string;
+    export default ResultBlockTitle;
+}
+declare module "@salesforce/label/c.Sample_Request_Form_URL" {
+    var Sample_Request_Form_URL: string;
+    export default Sample_Request_Form_URL;
+}
+declare module "@salesforce/label/c.SearchBlockTitle" {
+    var SearchBlockTitle: string;
+    export default SearchBlockTitle;
+}
+declare module "@salesforce/label/c.SearchBtn" {
+    var SearchBtn: string;
+    export default SearchBtn;
+}
+declare module "@salesforce/label/c.SearchingBtn" {
+    var SearchingBtn: string;
+    export default SearchingBtn;
+}
+declare module "@salesforce/label/c.TeamMemberRole" {
+    var TeamMemberRole: string;
+    export default TeamMemberRole;
+}
+declare module "@salesforce/label/c.TooManyRowsError" {
+    var TooManyRowsError: string;
+    export default TooManyRowsError;
+}
+declare module "@salesforce/label/c.TooManyRowsSummary" {
+    var TooManyRowsSummary: string;
+    export default TooManyRowsSummary;
+}
+declare module "@salesforce/label/c.TooManyRowsTitle" {
+    var TooManyRowsTitle: string;
+    export default TooManyRowsTitle;
+}
+declare module "@salesforce/label/c.TotalFooterLabel" {
+    var TotalFooterLabel: string;
+    export default TotalFooterLabel;
+}
+declare module "@salesforce/label/c.Upload_File" {
+    var Upload_File: string;
+    export default Upload_File;
+}
+declare module "@salesforce/label/c.ViewSearchSection" {
+    var ViewSearchSection: string;
+    export default ViewSearchSection;
+}
+declare module "@salesforce/label/c.WebSite_Form_URL" {
+    var WebSite_Form_URL: string;
+    export default WebSite_Form_URL;
+}
+declare module "@salesforce/label/c.creditApplicatiobFromEmail" {
+    var creditApplicatiobFromEmail: string;
+    export default creditApplicatiobFromEmail;
+}
+declare module "@salesforce/label/c.priceBook_for_selection_sheet" {
+    var priceBook_for_selection_sheet: string;
+    export default priceBook_for_selection_sheet;
+}
+declare module "@salesforce/label/c.pricebook" {
+    var pricebook: string;
+    export default pricebook;
+}
